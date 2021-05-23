9ebb6c4c3e5be97b43aebb38fcbe9e3f71fb0348
Oleg Vaiser <aly9681082@gmail.com>
2021-05-23 17:24:00 +0300

    first commit

---
+++ first-service/src/test/java/com/weisser/firstservice/FirstServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weisser.firstservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
