f06cf660dc704ce954e063d710da67209b428bc3
Oleg Vaiser <aly9681082@gmail.com>
2021-05-24 18:40:08 +0300

    config java

---
+++ gateway-java/src/main/java/com/weisser/gatewayjava/GatewayJavaApplication.java
@@ -0,0 +1,13 @@
+package com.weisser.gatewayjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayJavaApplication.class, args);
+	}
+
+}
---
+++ gateway-java/src/test/java/com/weisser/gatewayjava/GatewayJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weisser.gatewayjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
